b0cde465e5e1973f03930c2fd73d5010e15984a7
Mohd Yasir Afaq <yasirafaq16@gmail.com>
2026-05-12 22:13:32 +0530

    Deleted extra folder

--- New folder/FiscalFLow-landingpage/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// Allow direct dashboard access from landing page buttons.
-const PrivateRoute = ({ children }) => {
-  return children
-}
-
-export default PrivateRoute
-
